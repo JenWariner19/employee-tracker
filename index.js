3e258e656a4745ba6f6882a1a64facf44c194a3a
Jen Wariner <jenniferwariner@yahoo.com>
2023-05-09 16:20:09 -0500

    added sql database and tables

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer');
+
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+
+    }
+]) 
+
+.then((answer) => {
+    
+})
